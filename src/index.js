b06c131d540c46f7ed93b61a09e7a7fba2f95d8e
Kim R <kimberleyrangel1@gmail.com>
2020-04-15 22:27:14 -0400

    almost done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('starting up')
+/* eslint-disable linebreak-style */
+// change require to es6 import style
+
+// import $ from 'jquery';
+import './style.scss';
+
+// eslint-disable-next-line linebreak-style
+// const $ = require('jquery');
+
+
+// $('#main').html('Here we go!');
+let num = 0;
+window.setInterval(() => {
+  num += 1;
+  document.getElementById('main').innerHTML = `You've been on this page for ${num} seconds...`;
+}, 1000);
